d60ffa9a7cc5b4c8bbcfe493f0d6461fcdc9254b
dlamarca1 <86493548+dlamarca1@users.noreply.github.com>
2021-08-31 19:10:19 -0300

    Add files via upload
    Java back-end part of the project

---
+++ meetingroom/src/main/java/com/dio/crud/meetingroom/meetingroom/exeptions/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.dio.crud.meetingroom.meetingroom.exeptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+}
---
+++ meetingroom/src/main/java/com/dio/crud/meetingroom/meetingroom/model/Room.java
@@ -0,0 +1,38 @@
+package com.dio.crud.meetingroom.meetingroom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "meetingroom")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+    @Override
+    public String toString() {
+        return "Room [id="+id+",name="+ name+",startHour="+ startHour+ "endHour="+ endHour+ " ]";
+    }
+}
